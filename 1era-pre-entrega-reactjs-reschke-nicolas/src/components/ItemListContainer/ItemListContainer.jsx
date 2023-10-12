ed3b8cc292eb26156e9b4bda4d984d304e2d6ece
Nicolas Reschke <nicolas.reschke@hotmail.com>
2023-10-12 17:43:45 -0300

    1st commit Reschke Nicolas

--- 1era-pre-entrega-reactjs-reschke-nicolas/src/components/ItemListContainer/ItemListContainer.jsx
+++ 1era-pre-entrega-reactjs-reschke-nicolas/src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,9 +5,9 @@ function ItemListContainer({saludo = "saludo por defecto"}) {
                 <p>{saludo}</p>
             </div>
             <div>
-                <img className="imgPastas" src="/assets/foto1.jpeg" />
-                <img className="imgPastas" src="/assets/foto2.jpeg" />
-                <img className="imgPastas" src="/assets/foto3.jpeg" />
+                <a href=""><img className="imgPastas" src="/assets/foto1.jpeg" /></a>
+                <a href=""><img className="imgPastas" src="/assets/foto2.jpeg" /></a>
+                <a href=""><img className="imgPastas" src="/assets/foto3.jpeg" /></a>
             </div>
         </>
     )
