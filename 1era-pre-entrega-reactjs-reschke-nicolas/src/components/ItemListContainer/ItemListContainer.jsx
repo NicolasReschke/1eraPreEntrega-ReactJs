dc3bcaa97c345b589fec995f72540a6c782696eb
Nicolas Reschke <nicolas.reschke@hotmail.com>
2023-11-02 14:01:34 -0300

    first commit

--- 1era-pre-entrega-reactjs-reschke-nicolas/src/components/ItemListContainer/ItemListContainer.jsx
+++ 1era-pre-entrega-reactjs-reschke-nicolas/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react"
+import { mFetch } from "../../helpers/mFetch"
+
 function ItemListContainer({saludo = "saludo por defecto"}) {
+    const [ products, setProducts ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect (()=>{
+        mFetch()
+        .then(resultado => setProducts(resultado))
+        .catch(error => console.log(error))
+        .finally(()=> setLoading(false))
+
+    }, [])
+console.log(products)
+
+
     return (
         <>
-            <div>
-                <p>{saludo}</p>
-            </div>
-            <div>
-                <a href=""><img className="imgPastas" src="/assets/foto1.jpeg" /></a>
-                <a href=""><img className="imgPastas" src="/assets/foto2.jpeg" /></a>
-                <a href=""><img className="imgPastas" src="/assets/foto3.jpeg" /></a>
-            </div>
+            <p className="parrafoBienvenida">{saludo}</p>
+            { loading ? <img src="/assets/sample.gif" alt="" />
+            :
+            products.map(product=> 
+                                    <div className="card w-25">
+                                        <img src={product.img} className="card-img-top" alt=""/>
+                                            <p>Nombre: {product.name}</p>
+                                            <p>Categoría: {product.category}</p>
+                                            <p>Precio: {product.price}</p>
+                                            <div className="card-footer">
+                                                <button className="btn w-100">Detalle</button>
+                                            </div> 
+                                    </div>
+            )}
         </>
     )
 }
